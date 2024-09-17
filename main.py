a6a1b9465d9ae3bfd4076651c6623a9a6b68befc
Rahul T Gerorge <34202955+rahul-george@users.noreply.github.com>
2024-09-17 21:22:57 +0000

    fix: fixed issue with json navigation

--- main.py
+++ main.py
@@ -3,19 +3,40 @@ import time
 import requests
 from collections import namedtuple
 
+DEBUG = 0
+
+if DEBUG == 1:
+    URLS = 'urls_debug'
+else:
+    URLS = 'urls'
+
+
+
 data_out = []
 data_invalid = []
 Record = namedtuple("Record", "Port Country Lat Lng Id AreaSize ShortName Region")
-InvalidRecords = namedtuple("InvalidRecords", "url")
+InvalidRecords = namedtuple("InvalidRecords", "url message")
 
-with open('urls', 'r') as fh:
-    while(url:=fh.readline()):
-        data_raw = requests.get(url.strip())
+index = 1
+with open(URLS, 'r') as fh:
+    while(url:=fh.readline().strip()):
+        url = url.replace(' ', '%20')
+        print(index, url)
+        data_raw = requests.get(url)
+        index += 1
         data_raw = data_raw.json()
+        if DEBUG: print(data_raw)
+        if not data_raw['features']:
+            invalid_record = InvalidRecords(url, 'Feature is None')
+            data_invalid.append(invalid_record._asdict())
+            continue
+
         if len(data_raw['features']) > 1: 
             print('data_raw: ', data_raw)
-            invalid_record = InvalidRecords(url)
+            invalid_record = InvalidRecords(url, 'multiple choices')
             data_invalid.append(invalid_record._asdict())
+            continue
+
         data = data_raw['features'][0]['properties']
         record = Record(data['port_name'],
                         data['country'],
@@ -26,13 +47,11 @@ with open('urls', 'r') as fh:
                         data['short_name'],
                         data['sub_region'])
         
-        print(record)
+        # print(record)
         data_out.append(record._asdict())
 
         time.sleep(1)
 
-
-print(data_out)
 
 with open('ports.csv', 'w', newline='') as csvfile:
     fieldnames = ["Port", "Country", "Lat", "Lng", "Id", "AreaSize", "ShortName", "Region"]
@@ -43,7 +62,7 @@ with open('ports.csv', 'w', newline='') as csvfile:
 
 
 with open('invalid_ports.csv', 'w', newline='') as csvfile:
-    fieldnames = ["url"]
+    fieldnames = ["url", "message"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
